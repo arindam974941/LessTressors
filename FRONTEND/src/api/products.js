6ba6703078337663ce8cf04ed73d9b814584072a
arindam974941 <arindammonda428@gmail.com>
2025-05-31 12:44:19 +0530

    after changing cors

--- FRONTEND/src/api/products.js
+++ FRONTEND/src/api/products.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // Your backend URL
+  baseURL: 'https://lesstressors.onrender.com', // Your backend URL
   withCredentials: true,
 });
 
